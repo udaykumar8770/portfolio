1a6e5d192e7efd3c1cdc2ad3b38fd32a848a7f6c
Uday <udaykumar8770@gmail.com>
2021-04-13 01:40:22 -0800

    first commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./images/logo.png";
 import FormatAlignJustifyIcon from "@material-ui/icons/FormatAlignJustify";
 
 const Nav = () => {
-  const [state, setState] = React.useState(true);
+  const [state, setState] = useState(false);
+  function myfun() {
+    if (!state) {
+      document.querySelector(".navbar__right").style.display = "flex";
+      setState(true);
+    } else {
+      document.querySelector(".navbar__right").style.display = "none";
+      setState(false);
+    }
+  }
   return (
     <nav className="navbar">
       <div className="container">
@@ -13,34 +22,31 @@ const Nav = () => {
               <p className="portfolio_logo">ᴍʏ ᴘᴏʀᴛꜰᴏʟɪᴏ</p>
             </div>
           </ul>
-          {state ? (
-            <ul className="navbar__right">
-              <li>
-                <a href="#">Home</a>
-              </li>
 
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Skills</a>
-              </li>
-              <li>
-                <a href="#">Portfolio</a>
-              </li>
-              <li>
-                <a href="#">Blog</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
-            </ul>
-          ) : (
-            ""
-          )}
+          <ul className="navbar__right">
+            <li>
+              <a href="#">Home</a>
+            </li>
+
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Skills</a>
+            </li>
+            <li>
+              <a href="#">Portfolio</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
         </div>
       </div>
-      <div className="toggle" onClick={() => setState(!state)}>
+      <div className="toggle" onClick={myfun}>
         <FormatAlignJustifyIcon />
       </div>
     </nav>
